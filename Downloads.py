71e06d74f39841d162f1ea578bb9c28d8709fec4
Programmer Xiaobai <85952769+158456645113@users.noreply.github.com>
2023-05-26 16:16:27 +0800

    Update Downloads.py
    mp4 download Single threaded
    
    Signed-off-by: Programmer Xiaobai <85952769+158456645113@users.noreply.github.com>

--- Downloads.py
+++ Downloads.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 import keyboard
-
+ 
 url_list = []
 
 
